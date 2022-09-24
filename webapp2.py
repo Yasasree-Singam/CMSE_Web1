76ed79970dfb28ed62f58e0bf5aa81319c011891
Yasasree-Singam <singam.yashu11@gmail.com>
2022-09-23 22:05:36 -0400

    Added python file(Home work 2) for getting the webpage

--- webapp2.py
+++ webapp2.py
@@ -3,5 +3,5 @@ import plotly.express as px
 import seaborn as sn
 df_iris = sn.load_dataset('iris')
 fig = px.scatter_3d(df_iris, x='sepal_length', y='sepal_width', z='petal_width',color='species',symbol='species')
-st.write("""# Describe the dataset""")
+st.write("""# 3d scatter plot for the IRIS dataset using plotly""")
 st.plotly_chart(fig)
